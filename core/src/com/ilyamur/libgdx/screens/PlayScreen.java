09a7f6dd605ad475d5aef398ed0f60f120016495
Ilya_Muravyev <ilya.yarn@gmail.com>
2016-06-01 00:41:40 +0300

    -

--- core/src/com/ilyamur/libgdx/screens/PlayScreen.java
+++ core/src/com/ilyamur/libgdx/screens/PlayScreen.java
@@ -1,7 +1,7 @@
 package com.ilyamur.libgdx.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -12,39 +12,34 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import com.ilyamur.libgdx.MarioBros;
 import com.ilyamur.libgdx.scenes.Hud;
 
-public class PlayScreen implements Screen {
+public class PlayScreen extends ScreenAdapter {
 
     private final MarioBros game;
     private final OrthographicCamera gameCamera;
     private final Viewport gamePort;
     private final Hud hud;
-    private final TmxMapLoader mapLoader;
-    private final TiledMap map;
     private final OrthogonalTiledMapRenderer renderer;
 
     public PlayScreen(MarioBros game) {
         this.game = game;
+
         gameCamera = new OrthographicCamera();
-        gamePort = new FitViewport(MarioBros.V_WIDTH / MarioBros.PPM, MarioBros.V_HEIGHT / MarioBros.PPM, gameCamera);
-        hud = new Hud(game.getBatch());
-        mapLoader = new TmxMapLoader();
-        map = mapLoader.load("level1.tmx");
-        renderer = new OrthogonalTiledMapRenderer(map, 1 / MarioBros.PPM);
+        gamePort = new FitViewport(ppm(MarioBros.V_WIDTH), ppm(MarioBros.V_HEIGHT), gameCamera);
         gameCamera.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+        hud = new Hud(game.getBatch());
+        renderer = new OrthogonalTiledMapRenderer(loadMap("level1.tmx"), ppm(1));
     }
 
-    @Override
-    public void show() {
+    private float ppm(float value) {
+        return value / MarioBros.PPM;
     }
 
-    private void handleInput(float dt) {
-        if (Gdx.input.isTouched()) {
-            gameCamera.position.x += 100 * dt;
-        }
+    private TiledMap loadMap(String fileName) {
+        return (new TmxMapLoader()).load(fileName);
     }
 
-    public void update(float dt) {
-        handleInput(dt);
+    public void update(float delta) {
         gameCamera.update();
         renderer.setView(gameCamera);
     }
@@ -65,21 +60,5 @@ public class PlayScreen implements Screen {
     @Override
     public void resize(int width, int height) {
         gamePort.update(width, height);
-    }
-
-    @Override
-    public void pause() {
-    }
-
-    @Override
-    public void resume() {
-    }
-
-    @Override
-    public void hide() {
-    }
-
-    @Override
-    public void dispose() {
     }
 }
