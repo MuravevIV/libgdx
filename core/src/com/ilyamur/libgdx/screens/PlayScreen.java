ab42b09e95ab0d8ac1b7a5ffdc287f3fd4eb6d6e
Ilya_Muravyev <ilya.yarn@gmail.com>
2016-06-01 00:27:58 +0300

    -

--- core/src/com/ilyamur/libgdx/screens/PlayScreen.java
+++ core/src/com/ilyamur/libgdx/screens/PlayScreen.java
@@ -4,156 +4,82 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.ilyamur.libgdx.MarioBros;
 import com.ilyamur.libgdx.scenes.Hud;
 
-/**
- * Created by brentaureli on 8/14/15.
- */
 public class PlayScreen implements Screen {
-    //Reference to our Game, used to set Screens
-    private MarioBros game;
-    private TextureAtlas atlas;
-    public static boolean alreadyDestroyed = false;
 
-    //basic playscreen variables
-    private OrthographicCamera gamecam;
-    private Viewport gamePort;
-    private Hud hud;
-
-    //Tiled map variables
-    private TmxMapLoader maploader;
-    private TiledMap map;
-    private OrthogonalTiledMapRenderer renderer;
-
-    //Box2d variables
-    private World world;
-    private Box2DDebugRenderer b2dr;
-
+    private final MarioBros game;
+    private final OrthographicCamera gameCamera;
+    private final Viewport gamePort;
+    private final Hud hud;
+    private final TmxMapLoader mapLoader;
+    private final TiledMap map;
+    private final OrthogonalTiledMapRenderer renderer;
 
     public PlayScreen(MarioBros game) {
-
         this.game = game;
-        //create cam used to follow mario through cam world
-        gamecam = new OrthographicCamera();
-
-        //create a FitViewport to maintain virtual aspect ratio despite screen size
-        gamePort = new FitViewport(MarioBros.V_WIDTH / MarioBros.PPM, MarioBros.V_HEIGHT / MarioBros.PPM, gamecam);
-
-        //create our game HUD for scores/timers/level info
+        gameCamera = new OrthographicCamera();
+        gamePort = new FitViewport(MarioBros.V_WIDTH / MarioBros.PPM, MarioBros.V_HEIGHT / MarioBros.PPM, gameCamera);
         hud = new Hud(game.getBatch());
-
-        //Load our map and setup our map renderer
-        maploader = new TmxMapLoader();
-        map = maploader.load("level1.tmx");
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
         renderer = new OrthogonalTiledMapRenderer(map, 1 / MarioBros.PPM);
-
-        //initially set our gamcam to be centered correctly at the start of of map
-        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
-
-        //create our Box2D world, setting no gravity in X, -10 gravity in Y, and allow bodies to sleep
-        world = new World(new Vector2(0, -10), true);
-        //allows for debug lines of our box2d world.
-        b2dr = new Box2DDebugRenderer();
-
-    }
-
-    public TextureAtlas getAtlas() {
-        return atlas;
+        gameCamera.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
     }
 
     @Override
     public void show() {
+    }
 
-
+    private void handleInput(float dt) {
+        if (Gdx.input.isTouched()) {
+            gameCamera.position.x += 100 * dt;
+        }
     }
 
     public void update(float dt) {
-
-        //takes 1 step in the physics simulation(60 times per second)
-        world.step(1 / 60f, 6, 2);
-
-        //update our gamecam with correct coordinates after changes
-        gamecam.update();
-        //tell our renderer to draw only what our camera can see in our game world.
-        renderer.setView(gamecam);
-
+        handleInput(dt);
+        gameCamera.update();
+        renderer.setView(gameCamera);
     }
-
 
     @Override
     public void render(float delta) {
-        //separate our update logic from render
         update(delta);
 
-        //Clear the game screen with Black
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        //render our game map
         renderer.render();
 
-        //renderer our Box2DDebugLines
-        b2dr.render(world, gamecam.combined);
-
-        game.getBatch().setProjectionMatrix(gamecam.combined);
-        game.getBatch().begin();
-        game.getBatch().end();
-
-        //Set our batch to now draw what the Hud camera sees.
         game.getBatch().setProjectionMatrix(hud.getStage().getCamera().combined);
-
+        hud.getStage().draw();
     }
 
     @Override
     public void resize(int width, int height) {
-        //updated our game viewport
         gamePort.update(width, height);
-
-    }
-
-    public TiledMap getMap() {
-        return map;
-    }
-
-    public World getWorld() {
-        return world;
     }
 
     @Override
     public void pause() {
-
     }
 
     @Override
     public void resume() {
-
     }
 
     @Override
     public void hide() {
-
     }
 
     @Override
     public void dispose() {
-        //dispose of all our opened resources
-        map.dispose();
-        renderer.dispose();
-        world.dispose();
-        b2dr.dispose();
-    }
-
-    public Hud getHud() {
-        return hud;
     }
 }
